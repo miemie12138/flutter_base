5982acb2c73b20c79a03735c66b0943038f0f897
meimei <983945386@qq.com>
2025-10-17 18:55:40 +0800

    Delete flutter_base directory

--- flutter_base/lib/liberary.dart
+++
@@ -1,3 +0,0 @@
-library library;
-
-export 'src/base/base.dart';
--- flutter_base/lib/src/base/widget/my_refresh_lsit_viewmodel.dart
+++
@@ -1,9 +0,0 @@
-import 'my_refresh_list_widget.dart';
-
-class MyRefreshListViewModel<T> extends BaseRefreshListViewModel<T> {
-  @override
-  int get pageSize => 20;
-
-  @override
-  Map<String, dynamic> get pageParams => {"start": page, "limit": pageSize};
-}
